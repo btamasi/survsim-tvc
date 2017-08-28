34fd973a1892c76ada7e5c1a521d49669eeb0a32
btamasi <balint.tamasi@gmail.com>
2017-08-28 13:55:36 +0200

    plots for sim3

--- sim3.R
+++ sim3.R
@@ -1,4 +1,4 @@
-### Simulation 2:
+### Simulation 3:
 ### =============
 ### Generate survival times from a proportional hazards model with time-dependent covariates
 ### and a set of redundant covariates. Estimate the model.
@@ -45,3 +45,17 @@ tv_data_r <- tmerge(tv_data, tvc_r, id = id, xr.tv1 = tdc(time, xr4),
 ## fit the model
 fit <- coxph(Surv(tstart, tstop, event) ~ x1 + x2 + x.tv1 + x.tv2 + x.step + 
                xr1 + xr2 + xr.tv1 + xr.tv2 + xr.step, data = tv_data_r)
+
+## compare estimated parameters to the originals
+data.frame(bhat = fit$coefficients, se = sqrt(diag(fit$var)),
+           b = c(0.3, 0.3, -0.5, -0.3, 0.6, 0, 0, 0, 0, 0)) %>%
+  ggplot(aes(x = attr(fit$coefficients, "names"), y = b)) + 
+  geom_errorbar(aes(ymin=bhat-2*se, ymax=bhat+2*se), width = .05) +
+  geom_point(aes(y = bhat), color = "black") + geom_point(aes(y = b), color = "red")
+
+## compare estimated baseline survival curve to the original survival function
+newdat <- data.frame(id = 1, x1 = 0, x2 = 0, tstart = 0, tstop = 50, event = 0, 
+                     x.tv1 = 0, x.tv2 = 0, x.step = 0, xr1 = 0, xr2 = 0, 
+                     xr.tv1 = 0, xr.tv2 = 0, xr.step = 0)
+plot(survfit(fit, newdata = newdat))
+curve(exp(-0.06*x^1.5), from = 0, to = 50, add = TRUE, col = "red", lty = 2, lwd = 2) # Weibull survival curve
